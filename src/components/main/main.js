4d86424611e169cbeaee140f2869230eae1eb761
Laura-dn <56314608+Laura-dn@users.noreply.github.com>
2020-06-30 14:21:23 +0300

    Update main.js

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -8,18 +8,10 @@ function Main() {
                   et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
                   aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
                   dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
-                  officia deserunt mollit anim id est laborum.`;
+                  officia deserunt mollit anim id est laborum...`;
 
     return(
         <main className="col-6">
-            <article className="card mb-3">
-                <h3 className="card-header">Какая-то очень важная статья/новость</h3>
-                <div className="card-body">
-                    <img className="float-left mr-3" src={ imgSrc } alt="Card image" />
-                    { body }
-                </div>
-                <a href="#" className="text-right m-2"><span className="badge badge-success">READ MORE</span></a>
-            </article>
             <article className="card mb-3">
                 <h3 className="card-header">Какая-то очень важная статья/новость</h3>
                 <div className="card-body">
