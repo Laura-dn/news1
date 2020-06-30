b25ef0a615d1a0cbbea211241918c24b778fc681
Laura-dn <56314608+Laura-dn@users.noreply.github.com>
2020-06-30 13:57:20 +0300

    load

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -3,49 +3,57 @@ import React from "react";
 import "./main.css";
 
 function Main() {
-
+    const imgSrc = "https://zdnet3.cbsistatic.com/hub/i/r/2020/06/30/f0b4381f-f098-4555-9a3c-bddcc6049086/thumbnail/770x578/ffcaf7a8e06459610b64ceeda7b114d3/screenshot-2020-06-30-at-09-30-02.png";
 
     return(
         <main className="col-6">
-            <article className="card border-secondary mb-3">
-                <h4 className="card-header">Какая-то очень важная статья/новость</h4>
+            <article className="card mb-3">
+                <h3 className="card-header">Какая-то очень важная статья/новость</h3>
                 <div className="card-body">
+                    <img className="float-left mr-3" src={ imgSrc } alt="Card image" />
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
                     et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
                     aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
                     dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
                     officia deserunt mollit anim id est laborum.
                 </div>
+                <a href="#" className="text-right m-2"><span class="badge badge-success">READ MORE</span></a>
             </article>
-            <article className="card border-secondary mb-3">
-                <h4 className="card-header">Какая-то очень важная статья/новость</h4>
+            <article className="card mb-3">
+                <h3 className="card-header">Какая-то очень важная статья/новость</h3>
                 <div className="card-body">
+                    <img className="float-left mr-3" src={ imgSrc } alt="Card image" />
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
                     et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
                     aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
                     dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
                     officia deserunt mollit anim id est laborum.
                 </div>
+                <a href="#" className="text-right m-2"><span class="badge badge-success">READ MORE</span></a>
             </article>
-            <article className="card border-secondary mb-3">
-                <h4 className="card-header">Какая-то очень важная статья/новость</h4>
+            <article className="card mb-3">
+                <h3 className="card-header">Какая-то очень важная статья/новость</h3>
                 <div className="card-body">
+                    <img className="float-left mr-3" src={ imgSrc } alt="Card image" />
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
                     et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
                     aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
                     dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
                     officia deserunt mollit anim id est laborum.
                 </div>
+                <a href="#" className="text-right m-2"><span class="badge badge-success">READ MORE</span></a>
             </article>
-            <article className="card border-secondary mb-3">
-                <h4 className="card-header">Какая-то очень важная статья/новость</h4>
+            <article className="card mb-3">
+                <h3 className="card-header">Какая-то очень важная статья/новость</h3>
                 <div className="card-body">
+                    <img className="float-left mr-3" src={ imgSrc } alt="Card image" />
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
                     et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
                     aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
                     dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
                     officia deserunt mollit anim id est laborum.
                 </div>
+                <a href="#" className="text-right m-2"><span class="badge badge-success">READ MORE</span></a>
             </article>
         </main>
     );
